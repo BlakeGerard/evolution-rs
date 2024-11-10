bb67e5ae89013c7856d6c8be81973807901fb2c4
Blake Gerard <blake.t.gerard@gmail.com>
2024-11-10 12:44:30 -0600

    Actor evaluation

--- src/actor.rs
+++ src/actor.rs
@@ -1,6 +1,5 @@
 use std::fmt;
 
-#[derive(Clone)]
 enum ActorStatus {
     Clear = 0,
     Tournament = 1,
@@ -10,55 +9,61 @@ enum ActorStatus {
 
 impl fmt::Display for ActorStatus {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-       match self {
-           ActorStatus::Clear => write!(f, "Clear"),
-           ActorStatus::Tournament => write!(f, "Tournament"),
-           ActorStatus::Selected => write!(f, "Selected"),
-           ActorStatus::TournamentSelected => write!(f, "TournamentSelected"),
-       }
+        match self {
+            ActorStatus::Clear => write!(f, "Clear"),
+            ActorStatus::Tournament => write!(f, "Tournament"),
+            ActorStatus::Selected => write!(f, "Selected"),
+            ActorStatus::TournamentSelected => write!(f, "TournamentSelected"),
+        }
     }
 }
 
-#[derive(Clone)]
 pub struct Actor {
-	m : f32,
-	b : f32,
-	fitness : f32,
-	status : ActorStatus,
+    m: f32,
+    b: f32,
+    fitness: f32,
+    status: ActorStatus,
 }
 
 impl Actor {
     pub fn new() -> Self {
-	Actor {
-	    m : rand::random::<f32>(),
-	    b : rand::random::<f32>(),
-	    fitness : 0.0,
-	    status : ActorStatus::Clear,
-	}
+        Actor {
+            m: rand::random::<f32>(),
+            b: rand::random::<f32>(),
+            fitness: 0.0,
+            status: ActorStatus::Clear,
+        }
     }
 
-    pub fn response(&self, x : f32) -> f32 {
-	self.m * x + self.b
+    pub fn evaluate(&mut self, data: &Vec<(f32, f32)>) {
+        self.fitness = 0.0;
+        for sample in data {
+            let response = self.m * sample.0 + self.b;
+            // Squared error loss
+            let error = sample.1 - response;
+            let sqerror = error * error;
+            self.fitness += sqerror;
+        }
     }
 }
 
 impl Default for Actor {
     fn default() -> Self {
-	Actor::new()
+        Actor::new()
     }
 }
 
 impl fmt::Display for Actor {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-	write!(f, "Actor {{\n")?;
-	write!(f, "  m : {}\n", self.m)?;
-	write!(f, "  b : {}\n", self.b)?;
-	write!(f, "  fitness : {}\n", self.fitness)?;
-	write!(f, "  status : {}\n", self.status)?;
-	write!(f, "}}\n")
+        write!(f, "Actor {{\n")?;
+        write!(f, "  m : {}\n", self.m)?;
+        write!(f, "  b : {}\n", self.b)?;
+        write!(f, "  fitness : {}\n", self.fitness)?;
+        write!(f, "  status : {}\n", self.status)?;
+        write!(f, "}}\n")
     }
 }
 
-pub fn initial_population(size : usize) -> Vec<Actor> {
+pub fn random_population(size: usize) -> Vec<Actor> {
     std::iter::repeat_with(Actor::default).take(size).collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-
 mod actor;
 
-fn main() {
+const NUM_ITERS: usize = 100;
+const NUM_ACTORS: usize = 5;
 
-    // y = 1.0x + 1.0
-    let data : Vec<(f32, f32)> = vec![(0.0, 1.0), (1.0, 2.0), (2.0, 3.0)];
-
-    let actors = actor::initial_population(5);
-    for actor in &actors {
-	for pair in &data {
-	    let response = actor.response(pair.0);
-	    println!("Response: {} vs. {}", response, pair.1);
-	}
+fn evaluate_actors(actors: &mut Vec<actor::Actor>, data: &Vec<(f32, f32)>) {
+    for actor in actors {
+        actor.evaluate(&data);
     }
+}
+
+fn print_actors(actors: &Vec<actor::Actor>) {
+    for actor in actors {
+        println!("{}", actor);
+    }
+}
+
+fn evolution(data: &Vec<(f32, f32)>) {
+    let mut arena_A = actor::random_population(NUM_ACTORS);
+    evaluate_actors(&mut arena_A, data);
+    print_actors(&arena_A);
+}
+
+fn main() {
+    // y = 2.0x + 0.0
+    let data: Vec<(f32, f32)> = vec![(0.0, 0.0), (1.0, 2.0), (2.0, 4.0), (3.0, 6.0), (4.0, 8.0)];
+
+    let start = std::time::Instant::now();
+    evolution(&data);
+    let elapsed = start.elapsed();
+    println!("Evolution time: {}\n", elapsed.as_micros());
 }
